cdf86b06f12b54007bff117b435ac3225b8996ed
wendytim4 <109157790+wendytim4@users.noreply.github.com>
2024-02-13 22:09:01 +0000

    my portfolio

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,12 +10,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import devDotToIcon from "../images/socials/devdotto.svg";
 import envelopeIcon from "../images/socials/envelope.svg";
 import gitHubIcon from "../images/socials/github.svg";
 import instagramIcon from "../images/socials/instagram.svg";
 import linkedInIcon from "../images/socials/linkedin.svg";
-import mediumIcon from "../images/socials/medium.svg";
+
 import twitterIcon from "../images/socials/twitter.svg";
 import youTubeIcon from "../images/socials/youtube.svg";
 
